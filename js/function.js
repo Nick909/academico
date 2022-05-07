19b5b9b116ad5421cbd563f7aef24403c0fe2b83
Nick909 <nicholas.silva909@outlook.com>
2022-05-06 22:35:57 -0300

    atualisacao

---
+++ js/function.js
@@ -0,0 +1,19 @@
+//Menu mobile
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
+
+//inicialização Parallax
+$(document).ready(function(){
+    $('.parallax').parallax();
+});
+
+//inicialização Tabs
+$(document).ready(function(){
+    $('.tabs').tabs();
+  });
+
+//inicialização scrollSpy
+$(document).ready(function(){
+$('.scrollspy').scrollSpy();
+});  
